14ce01094beae4ef0a041b5a0933d63d46989436
Elliott Maguire <me@elliott-m.com>
2019-03-25 15:19:34 -0700

    get up to posting edits back

--- ergal/gui/app.py
+++ ergal/gui/app.py
@@ -11,7 +11,7 @@ import sqlite3
 from ergal.profile import Profile
 from ergal.utils import get_db
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 def create_app(test_config=None):
@@ -23,11 +23,11 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    @app.route('/')
+    @app.route('/', methods=['GET'])
     def index():
         return render_template('index.html')
 
-    @app.route('/profiles')
+    @app.route('/profiles', methods=['GET'])
     def profiles():
         cursor = get_db()[1]
 
@@ -43,13 +43,25 @@ def create_app(test_config=None):
         context = {'profiles': profiles}
         return render_template('profiles.html', **context)
 
-    @app.route('/profile/<name>')
-    def profile(name):
+    @app.route('/profile/<name>', methods=['GET'])
+    def view(name):
         profile = Profile(name)
         endpoints = profile.endpoints.items()
 
         context = {'profile': profile, 'endpoints': endpoints}
-        return render_template('profile.html', **context)
+        return render_template('view.html', **context)
+
+    @app.route('/profile/<name>/edit', methods=['GET', 'POST'])
+    def edit(name):
+        profile = Profile(name)
+
+        if request.method == 'POST':
+            pass
+
+        context = {
+            'profile': profile,
+            'endpoints': profile.endpoints.items()}
+        return render_template('edit.html', **context)
 
     return app
 
